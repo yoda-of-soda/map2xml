c15689a14a4bbc939d1edb76e850afa9142627db
Lucian I. Last <li@last.nl>
2021-12-06 12:00:08 +0100

    Add test all types

--- map2xml_test.go
+++ map2xml_test.go
@@ -85,3 +85,52 @@ func TestMarshalSimpleNoSort(t *testing.T) {
 		}
 	}
 }
+
+func TestAllTypes(t *testing.T) {
+	var value = map[string]interface{}{
+		"test": map[string]interface{}{
+			"num":    2,
+			"string": "hi",
+			"nil":    nil,
+			"slices": []map[string]interface{}{
+				{"xml_child_name": "slice", "one": 1},
+				{"two": 2},
+				{"three": 3},
+			},
+			"multi": map[string]interface{}{
+				"level": "value",
+			},
+		},
+	}
+	var expected = `<test>
+  <multi>
+    <level>value</level>
+  </multi>
+  <nil></nil>
+  <num>2</num>
+  <slices>
+    <slice>
+      <one>1</one>
+    </slice>
+    <slice>
+      <two>2</two>
+    </slice>
+    <slice>
+      <three>3</three>
+    </slice>
+  </slices>
+  <string>hi</string>
+</test>`
+
+	xml := New(value)
+	xml.WithIndent("", "  ")
+	xml.WithSortedKeys()
+	ans, err := xml.MarshalToString()
+	if err != nil {
+		t.Error(err)
+	}
+	if ans != expected {
+		t.Logf("expected:\n---\n'%s'\n---\nvalue:\n+++\n%s\n+++\n", expected, ans)
+		t.Fail()
+	}
+}
